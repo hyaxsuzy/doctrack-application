5c4501299f00f0a165e4cc2457a9f6fef92bdec7
FranchescaHabing <franchescahabingg@gmail.com>
2022-06-30 05:38:17 -0700

    print guide images

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12464,7 +12464,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54817" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54966" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
